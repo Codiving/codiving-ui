aaa6828372e136f02c0d30cfe84f01b61d5d0e03
Codiving <codivingmoon@gmail.com>
2022-06-19 14:38:48 +0900

    Stack ui 추가

---
+++ src/ui/Stack.tsx
@@ -0,0 +1,72 @@
+import styled from "@emotion/styled";
+import { memo } from "react";
+import { UIType } from "src/utils/common";
+import { shouldForwardProp } from "src/utils/emotion";
+import Box from "./Box";
+
+interface StackProps {
+  children?: React.ReactNode;
+  display?: "flex" | "inline-flex";
+  flexDirection?: React.CSSProperties["flexDirection"];
+  justifyContent?: React.CSSProperties["justifyContent"];
+  alignItems?: React.CSSProperties["alignItems"];
+  flexWrap?: React.CSSProperties["flexWrap"];
+  spacing?: UIType.Spacing;
+  width?: React.CSSProperties["width"];
+  height?: React.CSSProperties["height"];
+}
+
+const getSpacing = (
+  spacing?: UIType.Spacing,
+  flexDirection?: React.CSSProperties["flexDirection"]
+) => {
+  if (!spacing || !flexDirection) return {};
+
+  const direction = flexDirection.includes("column")
+    ? "marginTop"
+    : "marginLeft";
+
+  return {
+    "& :not(:first-of-type)": {
+      [direction]: spacing * 8
+    }
+  };
+};
+
+const Stack = styled(Box, {
+  shouldForwardProp: shouldForwardProp([
+    "display",
+    "flexDirection",
+    "justifyContent",
+    "alignItems",
+    "flexWrap",
+    "spacing"
+  ])
+})<StackProps>(
+  ({
+    display,
+    flexDirection,
+    justifyContent,
+    alignItems,
+    flexWrap,
+    spacing,
+    width,
+    height
+  }) => ({
+    display,
+    flexDirection,
+    justifyContent,
+    alignItems,
+    flexWrap,
+    width,
+    height,
+    ...getSpacing(spacing, flexDirection)
+  })
+);
+
+export default memo(Stack);
+
+Stack.defaultProps = {
+  display: "flex",
+  flexDirection: "column"
+};
