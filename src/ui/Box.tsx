1594c57dce3300ca2340ab06db4b4be19fdb0db2
Codiving <codivingmoon@gmail.com>
2022-06-19 14:38:42 +0900

    Box ui 추가

---
+++ src/ui/Box.tsx
@@ -0,0 +1,39 @@
+import styled from "@emotion/styled";
+import { memo } from "react";
+import { UIProps } from "src/utils/common";
+import { shouldForwardProp } from "src/utils/emotion";
+import { getBorder, getMargin, getPadding } from "src/utils/function";
+
+interface BoxProps extends UIProps {}
+
+const Box = styled("div", {
+  shouldForwardProp: shouldForwardProp([
+    "p",
+    "pl",
+    "pr",
+    "pt",
+    "pb",
+    "m",
+    "ml",
+    "mr",
+    "mt",
+    "mb",
+    "b",
+    "bl",
+    "br",
+    "bt",
+    "bb"
+  ])
+})<BoxProps>(({ p, pl, pr, pt, pb, m, ml, mr, mt, mb, b, bl, br, bt, bb }) => {
+  const padding = getPadding(p, pl, pr, pt, pb);
+  const margin = getMargin(m, ml, mr, mt, mb);
+  const border = getBorder(b, bl, br, bt, bb);
+
+  return {
+    ...padding,
+    ...margin,
+    ...border
+  };
+});
+
+export default memo(Box);
