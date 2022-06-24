90a874f11bfb8981b7e651d87e8411a3806ad43a
Codiving <codivingmoon@gmail.com>
2022-06-25 08:19:48 +0900

    add hex to rgba util function

--- src/utils/function.ts
+++ src/utils/function.ts
@@ -84,3 +84,21 @@ export const getBorder = (
     borderBottom
   };
 };
+
+export const convertHex = (hexCode: string, _opacity: number = 1) => {
+  const opacity = _opacity > 1 && _opacity <= 100 ? _opacity / 100 : _opacity;
+
+  let hex = hexCode.replace("#", "");
+
+  if (hex.length === 3) {
+    hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+  }
+
+  console.log("hex", hex);
+
+  const r = parseInt(hex.substring(0, 2), 16);
+  const g = parseInt(hex.substring(2, 4), 16);
+  const b = parseInt(hex.substring(4, 6), 16);
+
+  return "rgba(" + r + "," + g + "," + b + "," + opacity + ")";
+};
