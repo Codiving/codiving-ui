fd276e2b140c00142ff0392636cd7983c882d0dd
Codiving <codivingmoon@gmail.com>
2022-06-26 11:09:18 +0900

    Update index

---
+++ src/ui/index.ts
@@ -0,0 +1,6 @@
+import Box from "./Box";
+import Button from "./Button";
+import Stack from "./Stack";
+import Typography from "./Typography";
+
+export { Box, Stack, Typography, Button };
---
+++ ui/index.tsx
@@ -0,0 +1,1 @@
+export { Box, Button, Stack, Typography } from "../src/ui";
