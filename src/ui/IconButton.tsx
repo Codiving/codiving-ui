2a700332f0191be1476120a3e4fd680440835741
Codiving <codivingmoon@gmail.com>
2022-06-26 19:54:37 +0900

    Add IconButton ui

---
+++ src/ui/IconButton.tsx
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+import Button from "./Button";
+
+const IconButton = styled(Button)(() => ({
+  borderRadius: "50%",
+  padding: 10
+}));
+
+export default IconButton;
