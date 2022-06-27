2da7931af1ff1d43ce1788e0a1680dec05e97cfe
Codiving <codivingmoon@gmail.com>
2022-06-27 22:53:07 +0900

    Update Button ui (border)

--- src/ui/Button/index.tsx
+++ src/ui/Button/index.tsx
@@ -75,6 +75,7 @@ const ContainedButton = styled(CommonButton)<ButtonProps>(
     return {
       color: "white",
       backgroundColor: convertHex(color, 0.7),
+      border: `1px solid ${color}`,
       boxShadow:
         "rgb(0 0 0 / 20%) 0px 3px 1px -2px, rgb(0 0 0 / 14%) 0px 2px 2px 0px, rgb(0 0 0 / 12%) 0px 1px 5px 0px",
       "&:hover": {
