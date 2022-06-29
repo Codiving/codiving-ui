b93be35ea82cac1a786f1cb499fa69361008527e
Codiving <codivingmoon@gmail.com>
2022-06-30 01:07:44 +0900

    Routing 작업

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -22,13 +22,36 @@ const ROUTES: IRoutes[] = [
       {
         title: "color11",
         path: "color11",
-        component: <div>theme 111</div>,
-        children: []
+        component: (
+          <div>
+            theme 111
+            <Outlet />
+          </div>
+        ),
+        children: [
+          {
+            title: "1",
+            path: "1",
+            component: <div>theme 5555</div>,
+            children: []
+          },
+          {
+            title: "2",
+            path: "2",
+            component: <div>theme 666666</div>,
+            children: []
+          }
+        ]
       },
       {
         title: "color22",
         path: "color22",
-        component: <div>theme 222</div>,
+        component: (
+          <div>
+            theme 222
+            <Outlet />
+          </div>
+        ),
         children: [
           {
             title: "color33333",
@@ -71,11 +94,18 @@ const Router = () => {
 
     if (children.length) {
       return children.map((child, index) => {
+        const { path, component, children } = child;
+
+        if (children.length)
+          return (
+            <Route key={index} path={path} element={component}>
+              {gen(child)}
+            </Route>
+          );
         return <Fragment key={index}>{gen(child)}</Fragment>;
       });
     }
 
-    // console.log("# path", path);
     return <Route path={path} element={component}></Route>;
   };
 
@@ -85,7 +115,6 @@ const Router = () => {
         <Route path="/" element={<MainLayout />}>
           {ROUTES.map((routes, index) => {
             const { path, component } = routes;
-            console.log("path", path);
             return (
               <Route key={index} path={path} element={component}>
                 {gen(routes)}
@@ -93,58 +122,6 @@ const Router = () => {
             );
           })}
         </Route>
-        {/* <Route path="/" element={<MainLayout />}>
-          <Route
-            path="theme"
-            element={
-              <div>
-                theme <Outlet />
-              </div>
-            }
-          >
-            <Route
-              path="color11"
-              element={
-                <div>
-                  color11 <Outlet />
-                </div>
-              }
-            ></Route>
-            <Route
-              path="color22"
-              element={
-                <div>
-                  color22 <Outlet />
-                </div>
-              }
-            >
-              <Route
-                path="color33"
-                element={
-                  <div>
-                    color33 <Outlet />
-                  </div>
-                }
-              ></Route>
-              <Route
-                path="color44"
-                element={
-                  <div>
-                    color44 <Outlet />
-                  </div>
-                }
-              ></Route>
-            </Route>
-          </Route>
-          <Route
-            path="ui"
-            element={
-              <div>
-                ui <Outlet />
-              </div>
-            }
-          ></Route>
-        </Route> */}
       </Routes>
     </BrowserRouter>
   );
