aafd50ba18cde9bc0dcee05122191f951cb197ef
Codiving <codivingmoon@gmail.com>
2022-07-12 22:56:05 +0900

    Apply noPadding props in IconButton ui

--- src/ui/IconButton.tsx
+++ src/ui/IconButton.tsx
@@ -1,9 +1,9 @@
 import styled from "@emotion/styled";
 import Button from "./Button";
 
-const IconButton = styled(Button)(() => ({
+const IconButton = styled(Button)(({ noPadding }) => ({
   borderRadius: "50%",
-  padding: 10
+  padding: noPadding ? undefined : 10
 }));
 
 export default IconButton;
