09da1c64500c8222875b0904ea322d20fa5f2d6e
Codiving <codivingmoon@gmail.com>
2022-07-19 08:48:22 +0900

    Update CalendarView

--- src/components/DatePicker/components/DatePickerCalendarView.tsx
+++ src/components/DatePicker/components/DatePickerCalendarView.tsx
@@ -1,32 +1,60 @@
 import { Stack, Typography } from "../../../ui";
-import Grid from "../../../ui/Grid";
+import StackItem from "../../../ui/StackItem";
 import DateObject from "../../../utils/DateObject/DateObject";
+
+const getDayRange = (date: Date) => {
+  const result: Date[] = [];
+  const dObject = new DateObject(date);
+
+  let start = dObject.startOf("month").startOf("week");
+  const end = dObject.endOf("month").endOf("week");
+
+  do {
+    result.push(start.getDateObject);
+    start = start.add(1, "day");
+  } while (start.isSameOrBefore(end));
+
+  return result;
+};
 
 interface DatePickerCalendarViewProps {
   date: Date;
 }
 
-const gridTemplateColumns = `repeat(7, ${30}px)`;
-
 const WeekDateText = () => {
   const startOfWeek = new DateObject().startOf("week");
 
   return (
-    <Grid gridTemplateColumns={gridTemplateColumns} justifyContent="center">
+    <Stack label="WeekDateText">
       {[0, 1, 2, 3, 4, 5, 6].map(item => (
-        <Stack key={item} justifyContent="center">
-          <Typography>{startOfWeek.add(item, "day").format("dd")}</Typography>
-        </Stack>
+        <StackItem flex={1}>
+          <Typography component="p" textAlign="center">
+            {startOfWeek.add(item, "day").format("dd")}
+          </Typography>
+        </StackItem>
       ))}
-    </Grid>
+    </Stack>
   );
 };
 
 const DatePickerCalendarView = (props: DatePickerCalendarViewProps) => {
+  const { date } = props;
+
+  const dayRange = getDayRange(date);
+
   return (
-    <Stack>
+    <Stack label="DatePickerCalendarView" direction="column">
       <WeekDateText />
-      DatePickerCalendarView
+      <div style={{ display: "flex", flexWrap: "wrap" }}>
+        {dayRange.map(day => (
+          <div
+            style={{ width: `${100 / 7}%`, textAlign: "center" }}
+            key={day.toDateString()}
+          >
+            {new DateObject(day).format("DD")}
+          </div>
+        ))}
+      </div>
     </Stack>
   );
 };
