f866736344d6f92c4e870046b5441bc91f8ec224
Codiving <codivingmoon@gmail.com>
2022-07-19 19:17:09 +0900

    Update DayItem component

--- src/components/DatePicker/components/DatePickerCalendarView/DayContainer/DayItem.tsx
+++ src/components/DatePicker/components/DatePickerCalendarView/DayContainer/DayItem.tsx
@@ -1,15 +1,22 @@
 import styled from "@emotion/styled";
 import StackItem from "../../../../../ui/StackItem";
+import { shouldForwardProp } from "../../../../../utils/emotion";
 
-const DayItem = styled(StackItem)<{ color?: string }>(({ color }) => {
-  return {
-    cursor: "pointer",
-    color,
-    "&:hover": {
-      background: "green",
-      color: "white !important"
-    }
-  };
-});
+const DayItem = styled(StackItem, {
+  shouldForwardProp: shouldForwardProp(["width", "isSelected", "color"])
+})<{ color?: string; isSelected: boolean; width: number | string }>(
+  ({ color, isSelected, width }) => {
+    return {
+      width,
+      backgroundColor: isSelected ? "#a8a8e6" : undefined,
+      cursor: "pointer",
+      color,
+      "&:hover": {
+        background: "green",
+        color: "white !important"
+      }
+    };
+  }
+);
 
 export default DayItem;
