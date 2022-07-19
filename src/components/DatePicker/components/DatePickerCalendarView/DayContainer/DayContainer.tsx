335a90a7f7a925b3687a107bb88e63334ecdf0a0
Codiving <codivingmoon@gmail.com>
2022-07-19 19:17:18 +0900

    Update DayContainer component

--- src/components/DatePicker/components/DatePickerCalendarView/DayContainer/DayContainer.tsx
+++ src/components/DatePicker/components/DatePickerCalendarView/DayContainer/DayContainer.tsx
@@ -19,12 +19,15 @@ const getDayRange = (date: Date) => {
 
 interface DayContainerProps {
   date: Date;
+  onChange: (value: Date) => void;
 }
 
 const DayContainer = (props: DayContainerProps) => {
-  const { date } = props;
+  const { date, onChange } = props;
 
   const dayRange = getDayRange(date);
+
+  const yyymmdd = new DateObject(date).format("YYYYMMDD");
 
   return (
     <Stack flexWrap="wrap">
@@ -32,6 +35,7 @@ const DayContainer = (props: DayContainerProps) => {
         const sunday = index % 7 === 0;
         const saturday = index % 7 === 6;
         const color = sunday ? "red" : saturday ? "blue" : undefined;
+        const _yyyymmdd = new DateObject(day).format("YYYYMMDD");
 
         return (
           <DayItem
@@ -39,6 +43,10 @@ const DayContainer = (props: DayContainerProps) => {
             width={`${100 / 7}%`}
             padding="14px 8px"
             color={color}
+            isSelected={yyymmdd === _yyyymmdd}
+            onClick={() => {
+              onChange(day);
+            }}
           >
             <Typography component="p" textAlign="center">
               {new DateObject(day).format("DD")}
